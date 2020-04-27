280477a918f1c1e6e183db3fe0dbd06f688c38ea
Kenneth Lee <kennethlee.id@gmail.com>
2020-04-27 19:03:50 -0400

    Add initial datagram struct

---
+++ datagram.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <stdint.h>
+
+#define MAX_NUM_SERVERS 5
+#define MAX_MSG_LEN 200
+
+/*----- Message Types -----*/
+enum datagram_type {
+    appendEntries=0,
+    requestVote,
+    appendEntriesACK,
+    requestVoteACK
+};
+
+struct datagram {
+    datagram_type type;
+    
+    uint8_t id;
+    uint16_t term;
+    uint16_t log_index;
+    uint16_t log_term;
+    uint16_t leader_commit;
+
+    uint16_t text_data_len;
+    char text_data[MAX_MSG_LEN]; //MAX_MSG_LEN is defined to be 200
+
+    uint16_t term_ack;
+    bool success_ack;
+};
